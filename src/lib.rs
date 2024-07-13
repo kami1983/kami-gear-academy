5208f39af0933c0a29f7a88cbf51476d4b1029a1
kami1983 <kami@cancanyou.com>
2024-07-14 07:51:10 +0800

    Update base struct.

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![no_std]
+
+use gstd::msg;
+use pebbles_game_io::*;
+
+static mut PEBBLES_GAME: Option<GameState> = None;
+
+#[no_mangle]
+extern "C" fn init() {
+    // YOUR CODE HERE
+}
+
+#[no_mangle]
+extern "C" fn handle() {
+    // YOUR CODE HERE
+}
+
+#[no_mangle]
+extern "C" fn state() {
+    // YOUR CODE HERE
+}
